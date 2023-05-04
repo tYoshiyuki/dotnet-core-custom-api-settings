f1732bf454cef1050e4891eeeac921f8783c0c6a
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-05-04 16:02:32 +0900

    update .NET6に更新。

--- NamespaceRoutingConvention.cs
+++ NamespaceRoutingConvention.cs
@@ -4,12 +4,16 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace WebApi2ToCore
 {
+    /// <summary>
+    /// 名前空間を用いてコントローラのアクションを設定するためのクラスです。
+    /// </summary>
     public class NamespaceRoutingConvention : IApplicationModelConvention
     {
         public void Apply(ApplicationModel application)
         {
             foreach (var controller in application.Controllers)
             {
+                // NOTE ルート属性が存在している場合は、そちらを優先します。
                 var hasAttributeRouteModels = controller.Selectors.Any(selector => selector.AttributeRouteModel != null);
 
                 if (!hasAttributeRouteModels)
